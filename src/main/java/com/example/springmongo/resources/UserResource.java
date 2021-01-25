614393e6e86970bbb480d12acda2e05d7f03638d
Artur Sousa <artursousa505@gmail.com>
2021-01-25 15:59:14 -0300

    connection do MongoDB

--- src/main/java/com/example/springmongo/resources/UserResource.java
+++ src/main/java/com/example/springmongo/resources/UserResource.java
@@ -1,26 +1,26 @@
 package com.example.springmongo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.springmongo.domain.User;
+import com.example.springmongo.service.UserService;
 
 @RestController
 @RequestMapping(value="/users")
-public class UserResources {
+public class UserResource {
+	
+	@Autowired
+	private UserService userService;
 	
 	@RequestMapping(method=RequestMethod.GET)
 	public ResponseEntity<List<User>> findAll() {
-		User nUser0 = new User("1", "example", "email@domain.com");
-		User nUser1 = new User("2", "example", "email@domain.com");
-		List<User> userList = new ArrayList<>();
-		userList.addAll(Arrays.asList(nUser0, nUser1));
+		List<User> userList = userService.findAll();
 		return ResponseEntity.ok().body(userList);
 	}
 	
